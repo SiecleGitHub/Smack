04535a36fccce83b7a112b623675af1313b22f4a
clemenssietas <siecle@gmx.de>
2018-08-09 15:11:29 +0200

    lesson 11

--- app/src/main/java/com/slopestyle/smack/Services/AuthService.kt
+++ app/src/main/java/com/slopestyle/smack/Services/AuthService.kt
@@ -3,14 +3,23 @@ package com.slopestyle.smack.Services
 import android.content.Context
 import android.util.Log
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.slopestyle.smack.Utilities.URL_LOGIN
 import com.slopestyle.smack.Utilities.URL_REGISTER
+import org.json.JSONException
 import org.json.JSONObject
 
 object AuthService {
+    var isLoggedIn = false
+    var userEmail = ""
+    var authToken = ""
 
-    fun registerUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit)  {
+    /*
+     Fully build out web request using Volley to register a user
+     */
+    fun registerUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit) {
 
         val jsonBody = JSONObject()
         jsonBody.put("email", email)
@@ -18,12 +27,18 @@ object AuthService {
         val requestbody = jsonBody.toString()
         println(URL_REGISTER)
 
-        val registerRequest = object : StringRequest(Method.POST, URL_REGISTER, Response.Listener { response ->
-            println(response)
-            complete(true)
-        }, Response.ErrorListener { error ->
-            Log.d("ERROR", "Could not register user: $error")
-        }) {
+        val registerRequest = object : StringRequest(
+                Method.POST,
+                URL_REGISTER,
+                Response.Listener { response ->
+                    println(response)
+                    complete(true)
+                },
+                Response.ErrorListener { error ->
+                    Log.d("ERROR", "Could not register user $email, $password: $error")
+                    complete(false)
+                }
+        ) {
             override fun getBodyContentType(): String {
                 return "application/json; charset=utf-8"
             }
@@ -34,10 +49,49 @@ object AuthService {
         }
 
         Volley.newRequestQueue(context).add(registerRequest)
-
     }
 
 
+    fun loginUser(context: Context, email: String, password: String, complete: (Boolean) -> Unit) {
+        val jsonBody = JSONObject()
+        jsonBody.put("email", email)
+        jsonBody.put("password", password)
+        val requestbody = jsonBody.toString()
+        println(URL_LOGIN)
+
+        val loginRequest = object : JsonObjectRequest(
+                Method.POST,
+                URL_LOGIN,
+                null,
+                Response.Listener {response ->
+                    // this is where we parse the json object
+                    try {
+                        userEmail = response.getString("user")
+                        authToken = response.getString("token")
+                        isLoggedIn = true
+                        complete(true)
+                    } catch (e: JSONException) {
+                        Log.d("JSON", "EXC" + e.localizedMessage)
+                        complete(false)
+                    }
+                } ,
+                Response.ErrorListener { error ->
+                    // this is where we deal with our error
+                    Log.d("ERROR", "Could not login user: $email, $password: $error")
+                    complete(false)
+                }
+        ) {
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getBody(): ByteArray {
+                return requestbody.toByteArray()
+            }
+        }
+
+        Volley.newRequestQueue(context).add(loginRequest)
+    }
 
 
 }
--- app/src/main/java/com/slopestyle/smack/Utilities/Constants.kt
+++ app/src/main/java/com/slopestyle/smack/Utilities/Constants.kt
@@ -1,5 +1,6 @@
 package com.slopestyle.smack.Utilities
 
 const val BASE_URL = "https://chattychatsiecle.herokuapp.com/v1/"
-//const val BASE_URL = "http://localhost:3005/v1/"
+//const val BASE_URL = "http://10.0.2.2:3005/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
