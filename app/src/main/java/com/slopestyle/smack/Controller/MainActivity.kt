f5b2277ed549eae1fcc998725a8062daeae85a4c
clemenssietas <siecle@gmx.de>
2018-08-19 18:31:49 +0200

    lesson 18

--- app/src/main/java/com/slopestyle/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/slopestyle/smack/Controller/MainActivity.kt
@@ -14,12 +14,16 @@ import android.widget.EditText
 import com.slopestyle.smack.R
 import com.slopestyle.smack.Services.AuthService
 import com.slopestyle.smack.Services.UserDataService
+import com.slopestyle.smack.Utilities.SOCKET_URL
 import com.slopestyle.smack.Utilities.USER_DATA_CHANGED_BROADCAST
+import io.socket.client.IO
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val socket = IO.socket(SOCKET_URL)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +34,25 @@ class MainActivity : AppCompatActivity() {
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
-        hideKeyboard()
+    }
 
+    override fun onResume() {
+        super.onResume()
         LocalBroadcastManager.getInstance(this).registerReceiver(
                 userDataChangedReceiver,
                 IntentFilter(USER_DATA_CHANGED_BROADCAST)
         )
+        socket.connect()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(userDataChangedReceiver)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        socket.disconnect()
     }
 
     val userDataChangedReceiver = object : BroadcastReceiver() {
@@ -89,18 +106,17 @@ class MainActivity : AppCompatActivity() {
                         val channelDesc = descTextField.text.toString()
 
                         // Create channel with the channel name and description
-                        hideKeyboard()
+                        socket.emit("newChannel", channelName, channelDesc)
                     }
                     .setNegativeButton("Cancel") { dialog: DialogInterface?, which: Int ->
                         // cancel and close the dialog
-                        hideKeyboard()
                     }
                     .show()
         }
     }
 
     fun sendMsgBtnClicked(view: View) {
-
+        hideKeyboard()
     }
 
     fun hideKeyboard() {
