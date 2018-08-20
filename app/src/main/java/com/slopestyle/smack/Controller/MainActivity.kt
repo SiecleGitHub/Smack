88cc95065482cb57fc4c4c2283c66c055dc6094e
clemenssietas <siecle@gmx.de>
2018-08-20 09:56:28 +0200

    lesson 25

--- app/src/main/java/com/slopestyle/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/slopestyle/smack/Controller/MainActivity.kt
@@ -8,10 +8,12 @@ import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import com.slopestyle.smack.Adapters.MessageAdapter
 import com.slopestyle.smack.Model.Channel
 import com.slopestyle.smack.Model.Message
 import com.slopestyle.smack.R
@@ -31,11 +33,17 @@ class MainActivity : AppCompatActivity() {
 
     val socket = IO.socket(SOCKET_URL)
     lateinit var channelAdapter: ArrayAdapter<Channel>
+    lateinit var messageAdapter: MessageAdapter
     var selectedChannel : Channel? = null
 
     private fun setupAdapters() {
         channelAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, MessageService.channels)
         channel_list.adapter = channelAdapter
+
+        messageAdapter = MessageAdapter(this, MessageService.messages)
+        messageListView.adapter = messageAdapter
+        val layoutManager = LinearLayoutManager(this)
+        messageListView.layoutManager = layoutManager
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -106,7 +114,10 @@ class MainActivity : AppCompatActivity() {
             MessageService.getMessages(selectedChannel!!.id) {complete ->
                 if(complete) {
                     for(message in MessageService.messages) {
-                        println(message.message)
+                        messageAdapter.notifyDataSetChanged()
+                        if(messageAdapter.itemCount > 0) {
+                            messageListView.smoothScrollToPosition(messageAdapter.itemCount - 1)
+                        }
                     }
                 }
 
@@ -125,6 +136,8 @@ class MainActivity : AppCompatActivity() {
     fun loginBtnNavClicked(view: View) {
         if(App.prefs.isLoggedIn) {
             UserDataService.logout()
+            channelAdapter.notifyDataSetChanged()
+            messageAdapter.notifyDataSetChanged()
             userNameNavHeader.text = ""
             userEmailNavHeader.text = ""
             userImageNavHeader.setImageResource(R.drawable.profiledefault)
@@ -191,6 +204,8 @@ class MainActivity : AppCompatActivity() {
 
                     val newMessage = Message(msgBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
                     MessageService.messages.add(newMessage)
+                    messageAdapter.notifyDataSetChanged()
+                    messageListView.smoothScrollToPosition(messageAdapter.itemCount - 1)
                 }
             }
         }
